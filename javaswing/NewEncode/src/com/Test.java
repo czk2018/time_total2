770950aa29953521811a7025097199151fef71bd
middle_wind <middle_wind@126.com>
2018-09-20 19:12:02 +0800

    add test file

---
+++ javaswing/NewEncode/src/com/Test.java
@@ -0,0 +1,51 @@
+package com;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "669";
+		
+		Integer i = Integer.parseInt(str);
+		
+		Integer i_mod = i % 256;
+		Integer i_div = i / 256;
+
+		String str_mod = Integer.toHexString(i_mod).toUpperCase();
+		String str_div = Integer.toHexString(i_div).toUpperCase();
+		if (str_div.length() == 1)
+		{
+			str_div = "0" + str_div;
+		}
+		
+		int value = Integer.parseInt(str_mod, 16);
+		
+		System.out.println(value + " " + str_mod + " " + str_div);
+		
+		System.out.println(strTo16("123N8"));
+		
+		char c6 = 0x02;
+		char c5 = 0x9D;
+		
+		char c = (char) (c6 * 256 + c5);
+		System.out.println((int) c);
+	}
+
+	public static String strTo16(String s) {
+
+	    String str = "";
+
+	    for (int i = 0; i < s.length(); i++) {
+
+	        int ch = (int) s.charAt(i);
+
+	        String s4 = Integer.toHexString(ch);
+
+	        str = str + s4;
+
+	    }
+
+	    return str;
+
+	}
+}
